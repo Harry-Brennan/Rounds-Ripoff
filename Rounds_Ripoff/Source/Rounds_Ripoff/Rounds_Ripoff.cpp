4665338443b992c5daac06442f6b49cc94146a19
Harry Brennan <harrymb04@hotmail.co.uk>
2025-04-12 17:38:30 +0100

    Deleted project starting again.

--- Rounds_Ripoff/Source/Rounds_Ripoff/Rounds_Ripoff.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Rounds_Ripoff.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rounds_Ripoff, "Rounds_Ripoff" );
- 
