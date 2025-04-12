17494e5301d66899a5668df4f2165f3e467e2369
Harry Brennan <harrymb04@hotmail.co.uk>
2025-04-12 15:44:01 +0100

    Project Setup

---
+++ Rounds_Ripoff/Source/Rounds_Ripoff/Rounds_RipoffGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Rounds_RipoffGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARounds_RipoffGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARounds_RipoffGameMode();
+};
+
+
+
